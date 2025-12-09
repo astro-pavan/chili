9e209cab96b6e072b04677aec8f44e0cb892982f
astro-pavan <pt426@cam.ac.uk>
2025-12-09 15:25:16 +0000

    Can calculate single C_eq values

--- basalt_dissolution.py
+++ basalt_dissolution.py
@@ -1,4 +1,318 @@
 import numpy as np
-from scipy.interpolate import RectBivariateSpline
-from scipy.optimize import root
+from scipy.interpolate import RectBivariateSpline, CubicSpline
+from scipy.optimize import root, curve_fit, root_scalar
+import pandas as pd
 
+from parameters import *
+from legacy_interp2d import legacy_interp2d_wrapper
+interp2d = legacy_interp2d_wrapper
+
+# EQUILBRIUM
+
+# import thermodynamics data
+
+path_species = './database/species.csv'
+speciesNames = pd.read_csv(path_species)
+
+speciesDict  = {} 
+logKDict     = {}
+KeqFuncs     = {}
+
+T = np.array([])
+P = np.array([])
+
+for name, colName in zip(speciesNames['species name'], speciesNames['species col name']):
+    
+    data = pd.read_csv('./database/' + name + '_th', \
+                                usecols=['out.' + colName + '.T', \
+                                        'out.' + colName + '.P', \
+                                        'out.' + colName + '.logK'])
+    
+    if data['out.' + colName + '.logK'].isna().any():
+
+        def func(X, a, b, c, d, e):
+            x,y = X
+            return a * (x ** 3) + b * (x ** 2) + c * x + d * y + e
+        
+        guess = (0.5, 0.5, 0.5, 0.5, 0.5)
+
+        fitData = data.dropna()
+        colParams = {}
+
+        x = fitData['out.' + colName + '.T'].values
+        y = fitData['out.' + colName + '.P'].values
+        z = fitData['out.' + colName + '.logK'].values
+
+        params = curve_fit(func, (x, y), z, guess)
+
+        colParams['out.' + colName + '.logK'] = params[0]
+
+        x = data[pd.isnull(data['out.' + colName + '.logK'])]['out.' + colName + '.T'].values
+        y = data[pd.isnull(data['out.' + colName + '.logK'])]['out.' + colName + '.P'].values
+        n = data[pd.isnull(data['out.' + colName + '.logK'])].index.astype(float).values
+        
+        data['out.' + colName + '.logK'][n] = func((x, y), *colParams['out.' + colName + '.logK'])
+    
+    speciesDict[name] = data
+
+    T = np.unique(data['out.' + colName + '.T'].values)
+    P = np.unique(data['out.' + colName + '.P'].values)
+
+    logKDict[name] = np.reshape(data['out.' + colName + '.logK'].values, (len(P),len(T)))
+    
+KeqFuncs['woll'] = interp2d(T, P, 10**(      logKDict['Ca+2'] \
+                                        +   2*logKDict['HCO3-'] \
+                                        +     logKDict['SiO2'] \
+                                        -     logKDict['wollastonite']  \
+                                        -     logKDict['water'] \
+                                        -   2*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['enst'] = interp2d(T, P, 10**(      logKDict['Mg+2'] \
+                                        +   2*logKDict['HCO3-'] \
+                                        +     logKDict['SiO2'] \
+                                        -     logKDict['enstatite'] \
+                                        -     logKDict['water'] \
+                                        -   2*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['ferr'] = interp2d(T, P, 10**(      logKDict['Fe+2'] \
+                                        +   2*logKDict['HCO3-'] \
+                                        +     logKDict['SiO2'] \
+                                        -     logKDict['ferrosilite'] \
+                                        -     logKDict['water'] \
+                                        -   2*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['fors'] = interp2d(T, P, 10**(      logKDict['Mg+2'] \
+                                        +   2*logKDict['HCO3-'] \
+                                        + 1/2*logKDict['SiO2'] \
+                                        - 1/2*logKDict['forsterite'] \
+                                        -     logKDict['water'] \
+                                        -   2*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['faya'] = interp2d(T, P, 10**(      logKDict['Fe+2'] \
+                                        +   2*logKDict['HCO3-'] \
+                                        + 1/2*logKDict['SiO2'] \
+                                        - 1/2*logKDict['fayalite'] \
+                                        -     logKDict['water'] \
+                                        -   2*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['anor'] = interp2d(T, P, 10**(  1/2*logKDict['Ca+2'] \
+                                        +     logKDict['HCO3-'] \
+                                        + 1/2*logKDict['kaolinite'] \
+                                        - 1/2*logKDict['anorthite'] \
+                                        - 3/2*logKDict['water'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['kfel'] = interp2d(T, P, 10**(      logKDict['K+'] \
+                                        +     logKDict['HCO3-'] \
+                                        + 1/2*logKDict['kaolinite'] \
+                                        +   2*logKDict['SiO2'] \
+                                        -     logKDict['K-feldspar'] \
+                                        - 3/2*logKDict['water'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['albi'] = interp2d(T, P, 10**(      logKDict['Na+'] \
+                                        +     logKDict['HCO3-'] \
+                                        + 1/2*logKDict['kaolinite'] \
+                                        +   2*logKDict['SiO2'] \
+                                        -     logKDict['albite'] \
+                                        - 3/2*logKDict['water'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['musc'] = interp2d(T, P, 10**(      logKDict['K+'] \
+                                        +     logKDict['HCO3-'] \
+                                        + 3/2*logKDict['kaolinite'] \
+                                        -     logKDict['muscovite'] \
+                                        - 5/2*logKDict['water'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['phlo'] = interp2d(T, P, 10**(  1/3*logKDict['K+'] \
+                                        +     logKDict['Mg+2'] \
+                                        + 7/3*logKDict['HCO3-'] \
+                                        + 1/6*logKDict['kaolinite'] \
+                                        + 2/3*logKDict['SiO2'] \
+                                        - 1/3*logKDict['phlogopite'] \
+                                        - 7/6*logKDict['water'] \
+                                        - 7/3*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['anni'] = interp2d(T, P, 10**(  1/3*logKDict['K+'] \
+                                        +     logKDict['Fe+2'] \
+                                        + 7/3*logKDict['HCO3-'] \
+                                        + 1/6*logKDict['kaolinite'] \
+                                        + 2/3*logKDict['SiO2'] \
+                                        - 1/3*logKDict['annite'] \
+                                        - 7/6*logKDict['water'] \
+                                        - 7/3*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['anoh'] = interp2d(T, P, 10**(  1/2*logKDict['Ca+2'] \
+                                        +     logKDict['HCO3-'] \
+                                        + 1/2*logKDict['halloysite'] \
+                                        - 1/2*logKDict['anorthite'] \
+                                        - 3/2*logKDict['water'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['kfeh'] = interp2d(T, P, 10**(      logKDict['K+'] \
+                                        +     logKDict['HCO3-'] \
+                                        + 1/2*logKDict['halloysite'] \
+                                        +   2*logKDict['SiO2'] \
+                                        -     logKDict['K-feldspar'] \
+                                        - 3/2*logKDict['water'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['albh'] = interp2d(T, P, 10**(      logKDict['Na+'] \
+                                        +     logKDict['HCO3-'] \
+                                        + 1/2*logKDict['halloysite'] \
+                                        +   2*logKDict['SiO2'] \
+                                        -     logKDict['albite'] \
+                                        - 3/2*logKDict['water'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['mush'] = interp2d(T, P, 10**(      logKDict['K+'] \
+                                        +     logKDict['HCO3-'] \
+                                        + 3/2*logKDict['halloysite'] \
+                                        -     logKDict['muscovite'] \
+                                        - 5/2*logKDict['water'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['phlh'] = interp2d(T, P, 10**(  1/3*logKDict['K+'] \
+                                        +     logKDict['Mg+2'] \
+                                        + 7/3*logKDict['HCO3-'] \
+                                        + 1/6*logKDict['halloysite'] \
+                                        + 2/3*logKDict['SiO2'] \
+                                        - 1/3*logKDict['phlogopite'] \
+                                        - 7/6*logKDict['water'] \
+                                        - 7/3*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['annh'] = interp2d(T, P, 10**(  1/3*logKDict['K+'] \
+                                        +     logKDict['Fe+2'] \
+                                        + 7/3*logKDict['HCO3-'] \
+                                        + 1/6*logKDict['halloysite'] \
+                                        + 2/3*logKDict['SiO2'] \
+                                        - 1/3*logKDict['annite'] \
+                                        - 7/6*logKDict['water'] \
+                                        - 7/3*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['anth'] = interp2d(T, P, 10**(      logKDict['Mg+2'] \
+                                        +   2*logKDict['HCO3-'] \
+                                        + 8/7*logKDict['SiO2'] \
+                                        - 1/7*logKDict['anthophyllite'] \
+                                        - 6/7*logKDict['water'] \
+                                        -   2*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['grun'] = interp2d(T, P, 10**(      logKDict['Fe+2'] \
+                                        +   2*logKDict['HCO3-'] \
+                                        + 8/7*logKDict['SiO2'] \
+                                        - 1/7*logKDict['grunerite'] \
+                                        - 6/7*logKDict['water'] \
+                                        -   2*logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['bica'] = interp2d(T, P, 10**(      logKDict['H+'] \
+                                        +     logKDict['HCO3-'] \
+                                        -     logKDict['water'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+KeqFuncs['carb'] = interp2d(T, P, 10**(      logKDict['H+'] \
+                                        +     logKDict['CO3-2'] \
+                                        -     logKDict['HCO3-'] \
+                                        ), kind='linear')
+KeqFuncs['wate'] = interp2d(T, P, 10**(      logKDict['H+'] \
+                                        +     logKDict['OH-'] \
+                                        -     logKDict['water'] \
+                                        ), kind='linear')
+KeqFuncs['quar'] = interp2d(T, P, 10**(      logKDict['SiO2'] \
+                                        -     logKDict['quartz'] \
+                                        ), kind='linear')
+KeqFuncs['co2a'] = interp2d(T, P, 10**(      logKDict['CO2'] \
+                                        -     logKDict['carbon dioxide'] \
+                                        ), kind='linear')
+
+csvData  = pd.read_csv('./database/henry_diamond2003.csv')
+
+lnKHFunc = CubicSpline(csvData['T (K)'], csvData['ln (kH, MPa)'], extrapolate=True)
+
+K_H_Tb   = 10 / 55.5084 * np.exp(lnKHFunc(T)) * np.ones((len(P), len(T))) # convert MPa to bar
+Keq_CO2b = 1 / K_H_Tb
+KeqFuncs['co2b'] = interp2d(T, P, Keq_CO2b, kind='linear')
+
+K_H_Tc   = 1600 / 55.5084 * np.exp(-2400*(1/T - 1/298)) * np.ones((len(P), len(T)))
+Keq_CO2c = 1 / K_H_Tc
+KeqFuncs['co2c'] = interp2d(T, P, Keq_CO2c, kind='linear')
+
+# Calculate DIC and pH components for basalt dissolution
+
+x_CO2g_range = xCO2
+Temp_range = T
+Pres_range = P
+
+Pres, Temp, x_CO2g = np.meshgrid(Pres_range, Temp_range, x_CO2g_range)
+
+Temp = 280
+Pres = 1
+x_CO2g = 280e-6
+
+K_woll = KeqFuncs['woll'](Temp, Pres)
+K_enst = KeqFuncs['enst'](Temp, Pres)
+K_ferr = KeqFuncs['ferr'](Temp, Pres)
+K_anor = KeqFuncs['anoh'](Temp, Pres)
+K_albi = KeqFuncs['albh'](Temp, Pres)
+K_bica = KeqFuncs['bica'](Temp, Pres)
+K_carb = KeqFuncs['carb'](Temp, Pres)
+K_wate = KeqFuncs['wate'](Temp, Pres)
+K_co2a = KeqFuncs['co2a'](Temp, Pres)
+
+act_CO2_aq      =   x_CO2g * K_co2a
+a               =   2 * (K_woll ** 2) * K_carb
+b               =   (K_woll ** 2) * K_wate + K_woll**2 * K_bica * x_CO2g
+c               =   - (x_CO2g ** 2) * ((K_woll ** 2) * (K_bica ** 2) + (K_anor ** 4) * K_bica * K_albi)
+d               =   - (x_CO2g ** 3) * 2 * K_woll * (K_anor ** 2) * K_bica * (K_woll + K_enst + K_ferr)
+
+print(act_CO2_aq.shape)
+
+# system of equations to solve for the activity of HCO3- 
+
+def f(x):
+    return x**4 + b/a * x**3 + c/a * x + d/a
+
+def f1(x):
+    return 4 * x**3 + 3 * b/a * x**2 + c/a
+
+def f2(x):
+    return 12 * x**2 + 6 * b/a * x
+
+x0  = 10000 * (x_CO2g ** (1/2))
+
+sol = root_scalar(f, fprime=f1, fprime2=f2, x0=x0)
+print(sol)
+
+# x0 = newton(f, x, fprime=f1, args=(), tol=1e-6, maxiter=1000, fprime2=f2)
+
+act_HCO3_m      = sol.root
+
+act_CO3_mm      = K_carb / K_bica * act_HCO3_m**2 / x_CO2g
+
+alk             = 2 * act_CO3_mm + act_HCO3_m
+dic             = act_HCO3_m + act_CO2_aq + act_CO3_mm
+
+act_H_p         = K_bica * x_CO2g  / act_HCO3_m
+pH              = - np.log10(act_H_p)
+
+act_SiO2        = K_woll / K_anor**2
+
+act_DIV_pp      = (1 + K_enst/K_woll + K_ferr/K_woll) * K_anor**2 * (x_CO2g**2 / act_HCO3_m**2)
+act_MON_p       = K_anor**4 / K_woll**2 * (x_CO2g / act_HCO3_m)
+
+allSpecies = {'HCO3': act_HCO3_m,
+              'CO3': act_CO3_mm, 
+              'ALK': alk,
+              'DIC': dic, 
+              'pH': pH, 
+              'H': act_H_p,
+              'SiO2':act_SiO2, 
+              'DIV':act_DIV_pp, 
+              'MON':act_MON_p, 
+              'CO2':act_CO2_aq}
+
+print(allSpecies)
+
+# KINETICS
+
+# TRANSPORT
